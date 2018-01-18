ebf4ad5a795b52adfff39a265e0b55aa20bf4746
angrha <angrha@gmail.com>
2018-01-18 14:51:32 +0700

    add signup and cek login

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <a class="navbar-brand" href="#">Navbar</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -24,7 +24,7 @@
         <form class="form-inline my-2 my-lg-0">
           <input class="form-control mr-sm-2" placeholder="Search" type="text">
           <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-          <button type="button" class="btn btn-info">Log out</button>
+          <button type="button" class="btn btn-outline-secondary">Log out</button>
         </form>
       </div>
     </nav>
@@ -33,7 +33,6 @@
 
 <script>
 export default {
-
 }
 </script>
 
--- client/src/vuex/index.js
+++ client/src/vuex/index.js
@@ -1,21 +1,44 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
+import router from '../router'
 Vue.use(Vuex)
 
+const auth = 'tweet-auth'
 const baseUrl = 'http://localhost:3000'
 
 const store = new Vuex.Store({
   state: {
+    login: false
   },
   mutations: {
+    isLogin (state, payload) {
+      state.login = payload
+    }
   },
   actions: {
     signin ({ commit }, payload) {
       axios.post(baseUrl + '/api/users/signin', payload)
         .then(response => {
           console.log(response.data)
-          localStorage.setItem('authLogin', response.data.token)
+          localStorage.setItem(auth, response.data.token)
+          commit('isLogin', true)
+          // router.push({name: 'Tweets'})
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    },
+    checkLogin ({ commit }) {
+      if (localStorage.getItem(auth)) {
+        commit('isLogin', true)
+      }
+    },
+    signup ({commit}, payload) {
+      axios.post(baseUrl + '/api/users/signup', payload)
+        .then(response => {
+          alert('sign up berhasil')
+          router.push({name: 'Home'})
         })
         .catch(err => {
           console.log(err)
