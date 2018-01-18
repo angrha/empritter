4a0736ca361f0af9a9a2400ee235179551e9c6d7
angrha <angrha@gmail.com>
2018-01-18 13:41:53 +0700

    add user model

--- server/app.js
+++ server/app.js
@@ -1,14 +1,20 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+                  require('dotenv').config()
+mongoose.connect('mongodb://localhost/empritter'); //connect to database
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const index = require('./routes/index');
+const users = require('./routes/users');
+const posts = require('./routes/posts');
 
-var app = express();
+const app = express();
+app.use(cors())
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -19,7 +25,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/api/users', users);
+app.use('/api/posts', posts)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
