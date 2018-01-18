df9facd3c0bd224d5da7aadbf4d5fca736567179
angrha <angrha@gmail.com>
2018-01-18 14:06:18 +0700

    onprogres client side user login

---
+++ client/src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container col-md-4">
+    <div>
+      <div class="form-group">
+        <label for="exampleInputEmail1" class="lbl">Email address</label>
+        <input class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" type="email">
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1" class="lbl">Password</label>
+        <input class="form-control" id="exampleInputPassword1" placeholder="Password" type="password">
+      </div>
+      <router-link to="signup" type="button" class="btn btn-primary rgt">Sign up</router-link>
+      <button type="button" class="btn btn-success rgt">Login</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
